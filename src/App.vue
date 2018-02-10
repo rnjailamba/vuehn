6faf0b78f8ed8497e17072bf6c14250679f9fd11
Rnjai Lamba <rnjailamba12@gmail.com>
2018-02-10 16:12:01 -0600

    basic router application done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-link :to="{ name: 'Hello' }">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-link to="/coins/bitcoin">Bitcoin</router-link>    
     <router-view/>
   </div>
 </template>
